f0819731cc34e97d00ecbc70d22a0ed828ec18ed
Gaston Buet <34718932+buetgaston87@users.noreply.github.com>
2021-04-20 09:48:12 +0200

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,9 +3,7 @@
 # Return True if a given natural number is prime and False if it is not
 def is_prime(num):
     assert type(num) is int, "num is not an integer: %r" % num
-    assert num > 0, "num is not greater than zero: %r" % num
-    if num == 1:
-        return False    
+    assert num > 1, "num is not greater than one: %r" % num    
     for n in range(2,num):
         if num%n == 0:
             return False
@@ -16,7 +14,7 @@ def is_prime(num):
 # Return a list of all the prime numbers from 2 to the given number. The input must be a natural number
 def primes_number_generator(num):
     assert type(num) is int, "num is not an integer: %r" % num
-    assert num > 0, "num is not greater than zero: %r" % num
+    assert num > 1, "num is not greater than one: %r" % num
     primes = []
     for n in range(2,num+1):
         if is_prime(n):
