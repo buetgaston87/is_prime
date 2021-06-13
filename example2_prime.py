18f7241ff1a21c06c2f226f5a696f19a0ebc09f0
Gaston Buet <34718932+buetgaston87@users.noreply.github.com>
2021-06-13 21:19:36 +0200

    Update example2_prime.py

--- example2_prime.py
+++ example2_prime.py
@@ -2,12 +2,18 @@
 from primes import primes_number_generator
 
 def main():        
-    print("Input a natural number grater than 1 to know all the prime numbers from 2 to the given number, 0 (zero) or 1 (one) to exit.")
-    num = int(input("Number: "))
-    while num > 1:
-        print("List of primes numbers: ", end="")
-        print(primes_number_generator(num))
-        num = int(input("Number: "))
+    print("Input an integer number grater than 1 to know all the prime numbers from 2 to the given number. Ctrl+c to exit.")
+    try:
+        while True:    
+            try:
+                num = int(input("Number: "))
+                print("List of primes numbers: ", primes_number_generator(num))
+            except AssertionError as e:
+                print(e)
+            except Exception as e:
+                print(e)
+    except KeyboardInterrupt:
+        print("\nThe end!!")
 
 if __name__ == "__main__":
     main()
