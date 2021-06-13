fefcde0e6b5ca116b5a7b0f98543893bb7b4572f
Gaston Buet <34718932+buetgaston87@users.noreply.github.com>
2021-06-13 21:12:19 +0200

    Update example1_prime.py

--- example1_prime.py
+++ example1_prime.py
@@ -3,11 +3,18 @@ from primes import is_prime
 
 def main():    
     dct = {True: "prime", False: "composite"}
-    print("Input a natural number grater than 1 to know if it is prime, 0 (zero) or 1 (one) to exit.")
-    num = int(input("Number: "))
-    while num > 1:
-        print(str(num)+" is a "+dct[is_prime(num)]+" number.")
-        num = int(input("Number: "))
+    print("Input an integer number grater than 1 to know if it is prime. Ctrl+c to exit.")
+    try:
+        while True:    
+            try:
+                num = int(input("Number: "))
+                print(str(num)+" is a "+dct[is_prime(num)]+" number.")                
+            except AssertionError as e:
+                print(e)
+            except Exception as e:
+                print(e)
+    except KeyboardInterrupt:
+        print("\nThe end!!")
 
 if __name__ == "__main__":
     main()
